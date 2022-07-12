c707aff302fbe1429cc7474a7fb1bdcb4d019734
TurboAlex71 <hb24alex@gmail.com>
2022-07-12 18:44:01 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+//1 Переменные
+// // variable
+// // camelCase
+// var name = 'Frolov'
+// const firstName = 'Alex'
+// //const lastName = 'Frolov' // string
+// //const age = 30 // number
+// const isProgrammer = true // boolean
+//
+// console.log(isProgrammer)
+//
+// const _ = 'private'
+// const $ = 'some value'
+//
+// // const if = 'mkef' // err
+// const withNum5 = '5'
+// //const 5withNum = '5' // err
+//
+// // 2 Мутирование
+// //console.log('Имя человека: ' + firstName + ', а возраст человека: ' + age)//
+// //console.log(age.toString())//
+// //alert('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+// //const lastName = prompt('Ведите фамилию')
+// //alert(firstName +' ' + lastName)
+//
+// // 3 Операторы
+// let currentYear = 2022
+// const birthYear = 1992
+//
+// //const age = currentYear - birthYear
+// //console.log(age)
+//
+// const a = 10
+// const b = 5
+//
+// let c = 32
+// // //c = c + a
+// // c +=a
+// // c -=a
+// // c *=a
+// // c /=a
+// //
+// // console.log(a + b)
+// // console.log(a - b)
+// // console.log(a * b)
+// // console.log(a / b)
+// // //console.log(currentYear++)//
+// // console.log(++currentYear)
+// // //console.log(currentYear)//
+// // console.log(--currentYear)
+// // console.log(c)
+
+// 4 Типы данных
+const isProgrammet = true
+const name = 'Alex'
+const age = 30
+console.log(typeof isProgrammet)
+console.log(typeof name)
+console.log(typeof age)
